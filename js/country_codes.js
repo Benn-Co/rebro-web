f23b7895c3a3a878bed675b7587e1dd626595d06
Benn-Co <karitustephen@gmail.com>
2022-01-23 07:48:31 -0800

    Update country_codes.js

--- js/country_codes.js
+++ js/country_codes.js
@@ -10,52 +10,53 @@ function get_country_codes(lo,la) {
     processData: true,
     url: api_server_url + '/cordova/get_country_codes.php',
     success: function searchSuccess(response) {
-      try {
-         let countries =response.countries;   
-         let ccode =response.ccode; 
-         alert(countries.length);        
-      
-         /**let fLen = countries.length;
+      try {        
+         //alert(response.countries[0].cname);
+
+         let countries = response.countries;
+
+         let fLen = countries.length;
          
          for (let i = 0; i < fLen; i++) {
            if (i < 1) {
              $(".select_currency").html(countries[i].ccode);
-             $(".mcode").html(countries[i].mcode);
+             $(".mcode").html(countries[i].cname);
              localStorage.setItem("mcode", countries[i].mcode);
              localStorage.setItem("ccode", countries[i].ccode);
              localStorage.setItem("exrate", countries[i].exrate);
-      
-             var curre = '<li><a class="dropdown-item active currency_option" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].ccode + '</a></li>' +
+             localStorage.setItem("cname", countries[i].cname);
+
+             var curre = '<li><a class="dropdown-item active currency_option" cname="' + countries[i].cname + '" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].ccode + '</a></li>' +
                          '<li><hr class="dropdown-divider"></li>';
                          $(".currency_list").append(curre);
                          
-             var mcode_list = '<li><a class="dropdown-item currency_option" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].mcode + '</a></li>';
+             var mcode_list = '<li><a class="dropdown-item currency_option" cname="' + countries[i].cname + '" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].mcode + '</a></li>';
                          $(".mcode_list").append(mcode_list);
       
              $(".select_country").html(countries[i].name);
-             var country = '<li><a class="dropdown-item active country_option" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].name + '</a></li>' +
+             var country = '<li><a class="dropdown-item active country_option" cname="' + countries[i].cname + '" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].name + '</a></li>' +
                            '<li><hr class="dropdown-divider"></li>';
                            $(".country_list").append(country);            
            }else{
-             var curre = '<li><a class="dropdown-item currency_option" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].ccode + '</a></li>' +
+             var curre = '<li><a class="dropdown-item currency_option" cname="' + countries[i].cname + '" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].ccode + '</a></li>' +
                          '<li><hr class="dropdown-divider"></li>';
                          $(".currency_list").append(curre);
-             var mcode_list = '<li><a class="dropdown-item currency_option" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].mcode + '</a></li>';
+             var mcode_list = '<li><a class="dropdown-item currency_option" cname="' + countries[i].cname + '" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].mcode + '</a></li>';
                          $(".mcode_list").append(mcode_list);
       
-             var country = '<li><a class="dropdown-item active country_option" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].name + '</a></li>' +
+             var country = '<li><a class="dropdown-item active country_option" cname="' + countries[i].cname + '" mcode="' + countries[i].mcode + '" exchange_rate="' + countries[i].exrate + '" ccode="' + countries[i].ccode + '" currency_name="' + countries[i].ccode + '" country_name="' + countries[i].name + '" href="#">' + countries[i].name + '</a></li>' +
                            '<li><hr class="dropdown-divider"></li>';
                            $(".country_list").append(country);
            }
                             
-         } */
+         }
       } catch (error) {
-        alert(error);
+        //alert(error);
       }
 
     },
     error: function searchError(xhr, err) {
-      alert(err);
+      //alert(err);
       //mysnackbar("Error on ajax call: " + err  + " " + JSON.stringify(xhr));
     }
 });
